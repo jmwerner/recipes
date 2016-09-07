a4170bf0f726da9581378245e71bccf2c866a65c
Jeremy Werner <jmwerner1123@me.com>
2016-09-06 22:52:02 -0700

    Add automatic website page generation based on folder structure of input jsons from shiny app

--- website/webpageGenerator.py
+++ website/webpageGenerator.py
@@ -1,6 +1,15 @@
 
 import json
 import ast
+import subprocess
+
+category_tag = '<RECIPE_CATEGORY_GOES_HERE>'
+name_tag = '<RECIPE_NAME_GOES_HERE>'
+notes_tag = '<RECIPE_NOTES_GOES_HERE>'
+directions_tag = '<RECIPE_DIRECTIONS_GO_HERE>'
+ingredients_tag = '<RECIPE_INGREDIENTS_GO_HERE>'
+
+html_template_path = 'templates/recipeTemplate.html'
 
 def create_ingredients_html(ingredients):
     output_html_string = '<ul>' + '\n'
@@ -15,43 +24,54 @@ def create_ingredients_html(ingredients):
     output_html_string = output_html_string + '</ul>' + '\n'
     return output_html_string
 
-recipePath = '../allRecipes/Breakfast/FluffyPancakes.json'
-HTMLPath = 'templates/recipeTemplate.html'
-outputHTMLPath = 'templates/recipe_test.html'
+def remove_spaces(string):
+    return string.replace(' ', '')
 
 
-category_tag = '<RECIPE_CATEGORY_GOES_HERE>'
-name_tag = '<RECIPE_NAME_GOES_HERE>'
-notes_tag = '<RECIPE_NOTES_GOES_HERE>'
-directions_tag = '<RECIPE_DIRECTIONS_GO_HERE>'
-ingredients_tag = '<RECIPE_INGREDIENTS_GO_HERE>'
+def import_json(path):
+    with open(path, 'r') as recipe_file:
+        recipe = json.load(recipe_file)
+    recipe_file.close()
+    return recipe
+
+def import_html(path):
+    with open(path, 'r') as html_file:
+        html = html_file.read()
+    html_file.close()
+    return html
+
+def export_html(path, html_input):
+    with open(path, 'w') as html_output_file:
+        html_output_file.write(html_input)
+    html_output_file.close()
+
+################################################################################
+
+all_categories = subprocess.check_output(['ls', '../allRecipes']).decode("utf-8").split('\n')
+
+for recipe_category in all_categories:
+    if recipe_category != '':
+        all_recipes_in_category = subprocess.check_output(['ls', '../allRecipes/' + recipe_category]).decode("utf-8").split('\n')
+        for recipe_in_category in all_recipes_in_category:      
+            if recipe_in_category != '':
+                recipePath = '../allRecipes/' + recipe_category + '/' + recipe_in_category 
+
+                recipe = import_json(recipePath)
+                html = import_html(html_template_path)
+
+                output_html_path = 'allRecipes/' + remove_spaces(recipe['recipeCategory'][0]) + '/' + remove_spaces(recipe['recipeName'][0]) + '.html'
 
 
-with open(recipePath, 'r') as recipe_file:
-    recipe = json.load(recipe_file)
+                ingredients_html = create_ingredients_html(recipe['ingredients'])
 
-recipe_file.close()
+                html = html.replace(category_tag, recipe['recipeCategory'][0])
+                html = html.replace(name_tag, recipe['recipeName'][0])
+                html = html.replace(notes_tag, 'Notes: ' + recipe['notes'][0])
+                html = html.replace(directions_tag, recipe['directions'][0])
+                html = html.replace(ingredients_tag, ingredients_html)
 
+                subprocess.call(["mkdir", "-p", "allRecipes/" + remove_spaces(recipe['recipeCategory'][0])])
 
-with open(HTMLPath, 'r') as html_file:
-    html = html_file.read()
-
-html_file.close()
-
-
-ingredients_html = create_ingredients_html(recipe['ingredients'])
-
-html = html.replace(category_tag, recipe['recipeCategory'][0])
-html = html.replace(name_tag, recipe['recipeName'][0])
-html = html.replace(notes_tag, 'Notes: ' + recipe['notes'][0])
-html = html.replace(directions_tag, recipe['directions'][0])
-html = html.replace(ingredients_tag, ingredients_html)
-
-
-
-with open(outputHTMLPath, 'w') as html_output_file:
-  html_output_file.write(html)
-
-html_output_file.close()
+                export_html(output_html_path, html)
 
 
