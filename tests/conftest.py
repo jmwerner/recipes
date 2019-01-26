6f1f8a38afa60faa48534907738c1e45c09ebc16
Jeremy Werner <jmwerner1123@me.com>
2019-01-26 15:57:07 -0800

    Move helper functions into conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,23 +3,29 @@
 import os
 import pytest
 import bs4 as bs
+import subprocess
+import sys
+
+sys.path.insert(0, 'generator')
+import webpageGenerator as gen
+
 
 @pytest.fixture(scope="session")
 def sitemap_name():
     return 'sitemap.xml'
 
 @pytest.fixture(scope="session")
-def base_url():
-    return 'https://jmwerner.github.io/recipes/website/index.html'
+def root_directory():
+    return subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
 
 @pytest.fixture(scope="session")
 def xml_tag():
     return 'loc'
 
 @pytest.fixture(scope="session")
-def raw_sitemap(sitemap_name):
+def raw_sitemap(root_directory, sitemap_name):
     try:
-        with open(sitemap_name) as file:
+        with open(root_directory + '/' + sitemap_name) as file:
             sitemap = file.read().replace('\n', '')
     except ValueError:
         print('Error: Specified sitemap does not exist')
@@ -54,3 +60,83 @@ def strip_link(link_input, tag):
     link = link.replace('</' + tag + '>', '')
     link = link.strip()
     return link
+
+
+class Helpers:
+    @staticmethod
+    def get_html_from_url(root_directory, url):
+        # Replace base website with local path for fast reading
+        local_html = url.replace('http://jmwerner.github.io/recipes', root_directory)
+        with open(local_html, "r") as f:
+            page = f.read()
+        return page
+
+    @staticmethod
+    def create_category_iterator(ingredients_dict):
+        unique_categories = ['']
+        for i in range(0, len(ingredients_dict)):
+            if 'category' in ingredients_dict[i]:
+                if ingredients_dict[i]['category'][0] not in unique_categories:
+                    unique_categories.append(ingredients_dict[i]['category'][0])
+        if(len(unique_categories) == 1 and unique_categories[0] == ''):
+            unique_categories = ['noCategory']
+        output_dict = {}
+        for key in unique_categories:
+            output_dict[key] = 0
+        return output_dict
+
+    @staticmethod
+    def find_ingredient_category(input):
+        if 'category' not in input:
+            return 'noCategory'
+        else:
+            if not input['category'][0]:
+                return 'noCategory'
+            else:
+                return input['category'][0]
+
+    @staticmethod
+    def process_json_name(input_string):
+        output_string = input_string.strip(' \n').lower().title()
+        output_string = gen.lower_special_cases_in_string(output_string)
+        output_string = gen.replace_degrees_in_string(output_string)
+        # Replace html coded degree symbol with unicode symbol for comparison
+        output_string = output_string.replace('&#176', '°')
+        return output_string
+
+    @staticmethod
+    def process_json_number(input_number):
+        return gen.convert_to_mixed_number(input_number)
+
+    @staticmethod
+    def process_json_units(input_list):
+        plural = gen.string_to_float(input_list['number'][0]) > 1.0
+        processed_units = gen.set_plural_suffix(input_list['units'][0], plural)
+        return processed_units
+
+    def make_ingredient_dict_from_link(self, root_directory, link):
+        html = self.get_html_from_url(root_directory, link)
+        soup = bs.BeautifulSoup(html, 'lxml')
+        ingredient_names_from_html = soup.find_all('span', \
+            {'id': lambda L: L and L.startswith('recipeIngredient')})
+        ingredient_numbers_from_html = soup.find_all('span', \
+            {'id': lambda L: L and L.startswith('recipeNumber')})
+        ingredient_units_from_html = soup.find_all('span', \
+            {'id': lambda L: L and L.startswith('recipeUnit')})
+        ingredient_dict = {}
+        if ingredient_names_from_html:
+            for i in range(0, len(ingredient_names_from_html)):
+                ingredient_id = ingredient_names_from_html[i].get('id')
+                ingredient_id = ingredient_id.replace('recipeIngredient-', '')
+                ingredient_dict[ingredient_id] = \
+                    {'name': ingredient_names_from_html[i].text.strip(' \n'), \
+                    'number': ingredient_numbers_from_html[i].text.strip(' \n'), \
+                    'units': ingredient_units_from_html[i].text.strip(' \n'), \
+                    'value_tag':ingredient_numbers_from_html[i].get('value')}
+        return ingredient_dict
+
+
+
+@pytest.fixture(scope="session")
+def helpers():
+    return Helpers
--- tests/test_base_recipes.py
+++ tests/test_base_recipes.py
@@ -1,107 +1,36 @@
 '''Testing script for basic recipe conversion from json'''
 
-import sys
 import json
 import pytest
-import bs4 as bs
-
-sys.path.insert(0, 'generator')
-import webpageGenerator as gen
-
-def get_html_from_url(root_directory, url):
-    # Replace base website with local path for fast reading
-    local_html = url.replace('http://jmwerner.github.io/recipes', root_directory)
-    with open(local_html, "r") as f:
-        page = f.read()
-    return page
-
-def make_ingredient_dict_from_link(root_directory, link):
-    html = get_html_from_url(root_directory, link)
-    soup = bs.BeautifulSoup(html, 'lxml')
-    ingredient_names_from_html = soup.find_all('span', \
-        {'id': lambda L: L and L.startswith('recipeIngredient')})
-    ingredient_numbers_from_html = soup.find_all('span', \
-        {'id': lambda L: L and L.startswith('recipeNumber')})
-    ingredient_units_from_html = soup.find_all('span', \
-        {'id': lambda L: L and L.startswith('recipeUnit')})
-    ingredient_dict = {}
-    if ingredient_names_from_html:
-        for i in range(0, len(ingredient_names_from_html)):
-            ingredient_id = ingredient_names_from_html[i].get('id')
-            ingredient_id = ingredient_id.replace('recipeIngredient-', '')
-            ingredient_dict[ingredient_id] = \
-                {'name': ingredient_names_from_html[i].text.strip(' \n'), \
-                'number': ingredient_numbers_from_html[i].text.strip(' \n'), \
-                'units': ingredient_units_from_html[i].text.strip(' \n'), \
-                'value_tag':ingredient_numbers_from_html[i].get('value')}
-    return ingredient_dict
-
-def create_category_iterator(ingredients_dict):
-    unique_categories = ['']
-    for i in range(0, len(ingredients_dict)):
-        if 'category' in ingredients_dict[i]:
-            if ingredients_dict[i]['category'][0] not in unique_categories:
-                unique_categories.append(ingredients_dict[i]['category'][0])
-    if(len(unique_categories) == 1 and unique_categories[0] == ''):
-        unique_categories = ['noCategory']
-    output_dict = {}
-    for key in unique_categories:
-        output_dict[key] = 0
-    return output_dict
-
-def find_ingredient_category(input):
-    if 'category' not in input:
-        return 'noCategory'
-    else:
-        if not input['category'][0]:
-            return 'noCategory'
-        else:
-            return input['category'][0]
-
-def process_json_name(input_string):
-    output_string = input_string.strip(' \n').lower().title()
-    output_string = gen.lower_special_cases_in_string(output_string)
-    output_string = gen.replace_degrees_in_string(output_string)
-    # Replace html coded degree symbol with unicode symbol for comparison
-    output_string = output_string.replace('&#176', '°')
-    return output_string
-
-def process_json_number(input_number):
-    return gen.convert_to_mixed_number(input_number)
-
-def process_json_units(input_list):
-    plural = gen.string_to_float(input_list['number'][0]) > 1.0
-    processed_units = gen.set_plural_suffix(input_list['units'][0], plural)
-    return processed_units
 
 #########
 # Tests #
 #########
 
-def test_base_recipe_creation(root_directory, processed_links_from_sitemap):
+def test_base_recipe_creation(root_directory, processed_links_from_sitemap, helpers):
     for link in processed_links_from_sitemap:
-        ingredient_dict_from_html = make_ingredient_dict_from_link(root_directory, link)
+        ingredient_dict_from_html = helpers.make_ingredient_dict_from_link(helpers, root_directory, link)
         if ingredient_dict_from_html:
 
             json_link = link.replace('.html', '.json').replace('website/', '')
-            json_string = get_html_from_url(root_directory, json_link)
+            json_string = helpers.get_html_from_url(root_directory, json_link)
             ingredients_from_json = \
                 json.loads(json.loads(json_string)['ingredients'][0])
 
-            category_iterator = create_category_iterator(ingredients_from_json)
+            category_iterator = helpers.create_category_iterator(ingredients_from_json)
 
             for i in range(0, len(ingredient_dict_from_html)):
-                category = find_ingredient_category(ingredients_from_json[i])
+                category = helpers.find_ingredient_category(ingredients_from_json[i])
                 id = category + '-' + str(category_iterator[category])
                 category_iterator[category] += 1
 
                 processed_json_number = \
-                    process_json_number(ingredients_from_json[i]['number'][0])
-                processed_units = process_json_units(ingredients_from_json[i])
+                    helpers.process_json_number(ingredients_from_json[i]['number'][0])
+                processed_units = helpers.process_json_units(ingredients_from_json[i])
 
                 assert processed_json_number == \
                     ingredient_dict_from_html[id]['number']
-                assert process_json_name(ingredients_from_json[i]['name'][0]) \
+                assert helpers.process_json_name(ingredients_from_json[i]['name'][0]) \
                     == ingredient_dict_from_html[id]['name']
                 assert processed_json_number == \
                     ingredient_dict_from_html[id]['value_tag']
